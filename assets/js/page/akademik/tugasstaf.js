178a7d47cd1f679b42f1c250a04298da56cbba80
saka_mawa <sakadana2003@gmail.com>
2022-06-18 21:37:11 +0700

    Update Terbaru

--- assets/js/page/akademik/tugasstaf.js
+++ assets/js/page/akademik/tugasstaf.js
@@ -13,22 +13,6 @@ $(function() {
   });
   
   });
-
-  $(function() {
-    $("#kt_table_tugas_siswa2").on("click", function() {
-      $("#deleteall2").toggle($(this).find(".deletebox2:checked").length > 0);
-      if($(this).find(".deletebox2:checked").length < ($(this).find(".deletebox2").length)){
-          $('input[name="maincheckbox"]').prop('checked', false);
-      }else if($(this).find(".deletebox2:checked").length = ($(this).find(".deletebox2").length)){
-        $('input[name="maincheckbox"]').prop('checked', true);
-    }
-  })
-  $('input[name="maincheckbox"]').on("click", function() {
-      $('.deletebox2').prop('checked', this.checked);
-  });
-  
-  });
-  
   
   
   
--- modules/akademik/views/tugas_staf.php
+++ modules/akademik/views/tugas_staf.php
@@ -11,14 +11,14 @@
                         <div class="d-flex justify-content-end">
                             <button type="button" class="btn btn-sm btn-light-danger me-2 " id="deleteall" style="display: none;">
                                 <span class="svg-icon svg-icon-2 me-0">
-                                    <i class="fa-duotone fa-trash" style="font-size: 18px;"></i>
+                                    <i class="fa-duotone fa-ban" style="font-size: 18px;"></i>
                                 </span>
-                                Hapus</button>
-                            <button type="button" class="btn btn-sm btn-light-danger me-2 " id="newbutton" style="display: none;">
+                                Tolak Tugas</button>
+                            <button type="button" data-bs-toggle="modal" data-bs-target="#modalInputNilai" class="btn btn-sm btn-light-warning me-2 " id="newbutton" style="display: none;">
                                 <span class="svg-icon svg-icon-2 me-0">
-                                    <i class="fa-duotone fa-trash" style="font-size: 18px;"></i>
+                                    <i class="fa-duotone fa-feather" style="font-size: 18px;"></i>
                                 </span>
-                                Hapus2</button>
+                                Nilai Serentak</button>
                         </div>
                     </div>
                     <div class="fv-row">
@@ -220,7 +220,68 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="wrapper-tugas">
-                            <iframe src="<?= base_url('assets/demo_tugas.pdf') ?>" frameborder="0"></iframe>
+                            <!-- <iframe src="<?= base_url('assets/demo_tugas.pdf') ?>" frameborder="0"></iframe> -->
+                        </div>
+                    </div>
+                </div>
+                <div class="row my-4">
+                    <div class="d-flex justify-content-center">
+                        <button type="button" class="btn btn-primary">
+                            <!--begin::Svg Icon | path: icons/duotune/arrows/arr075.svg-->
+                            <span class="svg-icon svg-icon-2">
+                                <i class="fa-duotone fa-floppy-disk" style="font-size: 1.3rem;"></i>
+                            </span>
+                            <!--end::Svg Icon-->Download tugas
+                        </button>
+                        <!--end::Add user-->
+                    </div>
+                </div>
+            </div>
+            <!--end::Modal body-->
+        </div>
+        <!--end::Modal content-->
+    </div>
+    <!--end::Modal dialog-->
+</div>
+
+<div class="modal fade" id="modalInputNilai" tabindex="-1" aria-hidden="true">
+    <!--begin::Modal dialog-->
+    <div class="modal-dialog modal-dialog-centered mw-850px">
+        <!--begin::Modal content-->
+        <div class="modal-content">
+            <!--begin::Modal header-->
+            <div class="modal-header">
+                <!--begin::Modal title-->
+                <h2 class="fw-bolder">Input Nilai Siswa</h2>
+                <!--end::Modal title-->
+                <!--begin::Close-->
+                <div class="btn btn-icon btn-sm btn-active-icon-primary" data-bs-dismiss="modal" data-kt-users-modal-action="close" type="button">
+                    <!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
+                    <span class="svg-icon svg-icon-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="black" />
+                            <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="black" />
+                        </svg>
+                    </span>
+                    <!--end::Svg Icon-->
+                </div>
+                <!--end::Close-->
+            </div>
+            <!--end::Modal header-->
+            <!--begin::Modal body-->
+            <div class="modal-body scroll-y mx-10 my-2">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="d-flex flex-column mb-8 fv-row" id="req_password_ulang">
+                            <label class="d-flex align-items-center fs-6 fw-bold mb-2">
+                                <span class="required">Input Nilai Siswa</span>
+                            </label>
+                            <div class="position-relative d-flex align-items-center">
+                                <span class="svg-icon svg-icon-2 position-absolute mx-4">
+                                    <i class="fa-duotone fa-feather"" style="font-size: 1.3rem;"></i>
+                                </span>
+                                <input type="number" class="form-control form-control-solid ps-12" placeholder="Input Nilai Siswa" name="password_ulang">
+                            </div>
                         </div>
                     </div>
                 </div>
