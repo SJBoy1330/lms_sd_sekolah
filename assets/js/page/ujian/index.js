4f9f5192e7454c310d8032fdebbc2c62559d0c52
Dewafarin <69331869+Dewafarin@users.noreply.github.com>
2022-06-29 16:04:04 +0700

    baru

--- assets/js/page/ujian/index.js
+++ assets/js/page/ujian/index.js
@@ -1,13 +1,12 @@
-
-    $(window).on('load', function() {
-        $('.card-stretch').each(function(i) {
-            setTimeout(function() {
-                $('.card-stretch').eq(i).addClass('fadein-bottom');
-            }, 150 * i);
-        });
-        $('.three-card').each(function(i) {
-            setTimeout(function() {
-                $('.three-card').eq(i).addClass('fadein-bottom');
-            }, 150 * i);
-        });
+setTimeout(function() {
+    $('.card-stretch').each(function(i) {
+        setTimeout(function() {
+             $('.card-stretch').eq(i).addClass('fadein-bottom');
+        }, 150 * i);
     });
+    $('.three-card').each(function(i) {
+        setTimeout(function() {
+            $('.three-card').eq(i).addClass('fadein-bottom');
+        }, 150 * i);
+    });
+}, 150 );
