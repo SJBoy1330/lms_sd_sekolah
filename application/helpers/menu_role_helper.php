81595df1d27108cb7b109ebea2f8c368db285da4
saka_mawa <sakadana2003@gmail.com>
2022-07-26 18:16:03 +0700

    Push

--- application/helpers/menu_role_helper.php
+++ application/helpers/menu_role_helper.php
@@ -74,14 +74,7 @@ function get_menu_by_role()
 function get_dashboard()
 {
     $icon = '
-        <span class="svg-icon svg-icon-2x">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                <rect x="2" y="2" width="9" height="9" rx="2" fill="black" />
-                <rect opacity="0.3" x="13" y="2" width="9" height="9" rx="2" fill="black" />
-                <rect opacity="0.3" x="13" y="13" width="9" height="9" rx="2" fill="black" />
-                <rect opacity="0.3" x="2" y="13" width="9" height="9" rx="2" fill="black" />
-            </svg>
-        </span>
+        <i class="fa-duotone fa-grid-2" style="font-size: 2rem;"></i>
         ';
 
     return [
